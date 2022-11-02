64751a1c31bbfcf6d0d997e2679ede735dfa4e96
test <annac101@nycstudents.net>
2022-11-02 01:12:14 -0400

    we're almost there

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -3,16 +3,20 @@ public class LinearEquation {
   private int x2;
   private int y1;
   private int y2;
+  private double newx;
 
-  LinearEquation(int x1, int x2, int y1, int y2){
+  LinearEquation(int x1, int y1, int x2, int y2){
       this.x1 = x1;
       this.x2 = x2;
       this.y1 = y1;
       this.y2 = y2;
   }
 
+    public double roundedToHundredth(double toRound){
+        return Math.round(toRound*100.0)/100.0;
+    }
   public double slope() {
-      double slope1 = (double)((y2-y1)/(x2-x1));
+      double slope1 = (double)(y2-y1)/(x2-x1);
       return roundedToHundredth(slope1); //check??
   }
 
@@ -22,11 +26,11 @@ public class LinearEquation {
   }
 
   public String equation() {
-      String result = "y = ";
+      String result = "";
       int numerator = (y2-y1);
       int denominator = (x2-x1);
         if (y1 == y2) {
-          return "y = "+y1;
+          return "y ="+y1;
       }
       if ((double) numerator/denominator == 1) {
           result += "x";
@@ -44,10 +48,14 @@ public class LinearEquation {
               result += Math.abs(numerator) + "/" + Math.abs(denominator) + "x";
           }
       }
+      if (yIntercept() < 0) {
+          result += yIntercept();
+      }
+      if (yIntercept() > 0) {
+          result += "+"+yIntercept();
+      }
       return result;
       }
-
-
 
   public double distance() {
       double x = Math.pow((x2-x1),2);
@@ -55,6 +63,7 @@ public class LinearEquation {
       double dis = Math.sqrt(x+y);
       return roundedToHundredth(dis);
   }
+
   public String lineInfo() {
       return "The two points are: "+"(" +x1+", " + y1+")" + " and " + "("+x2 +", "+y2+")"+ "\n" +
               "The equation of the line between these points is: y = "+equation()+"\n"
@@ -62,9 +71,11 @@ public class LinearEquation {
      "The y-intercept of the line is: "+yIntercept()+"\n"+
       "The distance between the two points is: "+distance();
   }
-    public double roundedToHundredth(double toRound){
-      return Math.round(toRound*100.0)/100.0;
-    }
 
+    public double coordinateForX (double newx){
+      this.newx = newx;
+      double y = slope()*newx+yIntercept();
+      return roundedToHundredth(y);
+    }
 
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,30 +1,34 @@
 
 import java.util.Scanner;
 public class LinearEquationRunner {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome!");
-        System.out.println ("Enter coordinate 1:");
+        System.out.println("Welcome");
+        System.out.println("Enter coordinate 1:");
         String coord1 = scan.nextLine();
-        String x11 = coord1.substring(1,coord1.indexOf(","));
+        String x11 = coord1.substring(1, coord1.indexOf(","));
         int x1 = Integer.parseInt(x11);
-        String y11 = coord1.substring(coord1.indexOf(",")+1,coord1.indexOf(")"));
+        String y11 = coord1.substring(coord1.indexOf(",") + 1, coord1.indexOf(")"));
         int y1 = Integer.parseInt(y11);
-        System.out.println(x1);
-        System.out.println(y1);
+
         System.out.println("Enter coordinate 2: ");
         String coord2 = scan.nextLine();
-        String x22 = coord2.substring(1,coord2.indexOf(","));
+        String x22 = coord2.substring(1, coord2.indexOf(","));
         int x2 = Integer.parseInt(x22);
-        String y22 = coord2.substring(coord2.indexOf(",")+1,coord2.indexOf(")"));
+        String y22 = coord2.substring(coord2.indexOf(",") + 1, coord2.indexOf(")"));
         int y2 = Integer.parseInt(y22);
-        System.out.println(x2);
-        System.out.println(y2);
 
-        LinearEquation Linear = new LinearEquation(x1,x2,y1,y2);
-        System.out.println(Linear.lineInfo());
+        if (x1 == x2) {
+            System.out.println("\nThe point is on a vertical line: x = " + x1);
+        } else {
+            LinearEquation Linear = new LinearEquation(x1, y1, x2, y2);
+            System.out.println(Linear.lineInfo());
+            System.out.println("\nEnter a value for x: ");
+            double newx = scan.nextDouble();
+            System.out.println("\nThe point on the line is (" + newx + ","+Linear.coordinateForX(newx) + ")");
+        }
 
-        System.out.println("\nEnter a value for x: ");
-        int newx = scan.nextInt(); //finish thisss
     }
+
 }
+
